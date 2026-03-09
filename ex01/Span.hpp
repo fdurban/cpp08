9d53634267fadf0cc28a4e4b4dd0c80a5c0918ea
Fernando <ferbrizie@gmail.com>
2026-03-09 17:19:19 +0100

    continue with ex01

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,12 +1,23 @@
+#include <vector.h>
+
+
 
 
 class Span
 {
 	private:
 		unsigned int N;
+		std::vector<int> num;
 	public:
 		Span();
 		Span(unsigned int N);
-		Span();
-		void	addNumber();
+		Span(const Span &other)
+		void	addNumber(int number);
+		class myException: public std::exception
+		{
+			public:
+				const char* what() const throw();
+		}
+		unsigned int shortestSpan() const ;
+		unsigned int longestSpan() const ;
 }
